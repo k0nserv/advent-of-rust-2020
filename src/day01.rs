8631a7fe63d23851ee2e7551ccb55b6e5f6321c8
Hugo Tunius <h@tunius.se>
2020-12-01 23:26:45 +0000

    [Day 01] Solve day 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,22 +1,64 @@
-pub fn star_one(input: &str) -> i64 {
+use crate::parse_lines;
+
+pub fn star_one(input: &str) -> usize {
+    let sorted_numbers = {
+        let mut numbers: Vec<_> = parse_lines::<usize>(input).collect();
+        numbers.sort();
+        numbers
+    };
+
+    for x in &sorted_numbers {
+        for y in &sorted_numbers {
+            match x + y {
+                2020 => return x * y,
+                n @ _ if n > 2020 => break,
+                _ => (),
+            }
+        }
+    }
+
     0
 }
 
-pub fn star_two(input: &str) -> i64 {
+pub fn star_two(input: &str) -> usize {
+    let sorted_numbers = {
+        let mut numbers: Vec<_> = parse_lines::<usize>(input).collect();
+        numbers.sort();
+        numbers
+    };
+
+    for x in &sorted_numbers {
+        for y in &sorted_numbers {
+            for z in &sorted_numbers {
+                match x + y + z {
+                    2020 => return x * y * z,
+                    n @ _ if n > 2020 => break,
+                    _ => (),
+                }
+            }
+        }
+    }
+
     0
 }
 
 #[cfg(test)]
 mod tests {
     use super::{star_one, star_two};
+    const TEST_INPUT: &'static str = "1721
+979
+366
+299
+675
+1456";
 
     #[test]
     fn test_star_one() {
-        assert_eq!(star_one(""), 1)
+        assert_eq!(star_one(TEST_INPUT), 514579);
     }
 
     #[test]
     fn test_star_two() {
-        assert_eq!(star_two(""), 1)
+        assert_eq!(star_two(TEST_INPUT), 241861950);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -181,8 +181,8 @@ mod tests {
 
         let input = load_file("day01.txt");
 
-        assert_eq!(star_one(&input), 1);
-        assert_eq!(star_two(&input), 1);
+        assert_eq!(star_one(&input), 270144);
+        assert_eq!(star_two(&input), 261342720);
     }
 
     #[test]
