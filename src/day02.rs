c370ce2ec6bf96db060a874c4d6052f6c9bfd2e1
Hugo Tunius <h@tunius.se>
2020-12-02 18:27:56 +0000

    [Day 02] Solve day 2

--- src/day02.rs
+++ src/day02.rs
@@ -1,22 +1,135 @@
-pub fn star_one(input: &str) -> i64 {
-    0
+use std::ops::RangeInclusive;
+use std::str::FromStr;
+
+use crate::parse_lines;
+
+#[derive(Debug, Clone)]
+struct Policy {
+    required_char: char,
+    range: RangeInclusive<usize>,
 }
 
-pub fn star_two(input: &str) -> i64 {
-    0
+impl Policy {
+    fn is_valid_sled(&self, password: &str) -> bool {
+        let count = password.chars().fold(0, |acc, c| {
+            if self.required_char == c {
+                acc + 1
+            } else {
+                acc
+            }
+        });
+
+        self.range.contains(&count)
+    }
+
+    fn is_valid_toboggan(&self, password: &str) -> bool {
+        let count = password.chars().enumerate().fold(0, |acc, (idx, c)| {
+            match (
+                self.required_char == c,
+                *self.range.start() == idx + 1,
+                *self.range.end() == idx + 1,
+            ) {
+                (true, true, false) => acc + 1,
+                (true, false, true) => acc + 1,
+                _ => acc,
+            }
+        });
+
+        count == 1
+    }
+}
+
+impl FromStr for Policy {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut parts = s.trim().split_whitespace();
+        let range_part = parts.next();
+        let require_part = parts.next();
+
+        if range_part.is_none() || require_part.is_none() {
+            return Err(format!("Invalid policy definition `{}`", s));
+        }
+
+        let range_parts: Vec<_> = range_part
+            .unwrap()
+            .split('-')
+            .map(str::trim)
+            .map(|part| part.parse::<usize>().unwrap())
+            .collect();
+
+        if range_parts.len() != 2 {
+            return Err(format!(
+                "Invalid policy definition `{}`, expected exactly two parts of the range",
+                s
+            ));
+        }
+
+        Ok(Self {
+            required_char: require_part.unwrap().trim().chars().take(1).next().unwrap(),
+            range: RangeInclusive::new(range_parts[0], range_parts[1]),
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Entry {
+    policy: Policy,
+    password: String,
+}
+
+impl Entry {
+    fn is_valid_sled(&self) -> bool {
+        self.policy.is_valid_sled(&self.password)
+    }
+
+    fn is_valid_toboggan(&self) -> bool {
+        self.policy.is_valid_toboggan(&self.password)
+    }
+}
+
+impl FromStr for Entry {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<_> = s.split(':').collect();
+        if parts.len() != 2 {
+            return Err(format!("Invalid Entry `{}`", s));
+        }
+
+        let policy = parts[0].parse::<Policy>()?;
+        let password = parts[1].trim().to_owned();
+
+        Ok(Self { policy, password })
+    }
+}
+
+pub fn star_one(input: &str) -> usize {
+    parse_lines::<Entry>(input)
+        .filter(Entry::is_valid_sled)
+        .count()
+}
+
+pub fn star_two(input: &str) -> usize {
+    parse_lines::<Entry>(input)
+        .filter(Entry::is_valid_toboggan)
+        .count()
 }
 
 #[cfg(test)]
 mod tests {
     use super::{star_one, star_two};
+    const INPUT: &'static str = "1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc";
 
     #[test]
     fn test_star_one() {
-        assert_eq!(star_one(""), 1)
+        assert_eq!(star_one(INPUT), 2);
     }
 
     #[test]
     fn test_star_two() {
-        assert_eq!(star_two(""), 1)
+        assert_eq!(star_two(INPUT), 1)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -191,8 +191,8 @@ mod tests {
 
         let input = load_file("day02.txt");
 
-        assert_eq!(star_one(&input), 1);
-        assert_eq!(star_two(&input), 1);
+        assert_eq!(star_one(&input), 422);
+        assert_eq!(star_two(&input), 451);
     }
 
     #[test]
