5d239b67a33201558ef43db0e8cb4335aa114536
Hugo Tunius <h@tunius.se>
2020-12-09 22:23:10 +0000

    [Day 09] Solve day 9

--- src/day09.rs
+++ src/day09.rs
@@ -1,22 +1,76 @@
-pub fn star_one(input: &str) -> i64 {
-    0
+use std::collections::VecDeque;
+
+use itertools::Itertools;
+
+use crate::parse_lines;
+
+pub fn star_one(input: &str, window_size: usize) -> usize {
+    let numbers: Vec<usize> = parse_lines(input).collect();
+    let mut window: VecDeque<_> = numbers.iter().cloned().take(window_size).collect();
+
+    numbers
+        .iter()
+        .enumerate()
+        .skip(window_size)
+        .find(|&(i, n)| {
+            if window.iter().combinations(2).any(|v| v[0] + v[1] == *n) {
+                window.pop_front();
+                window.push_back(numbers[i]);
+
+                false
+            } else {
+                true
+            }
+        })
+        .map(|(_, n)| *n)
+        .unwrap_or(0)
 }
 
-pub fn star_two(input: &str) -> i64 {
-    0
+pub fn star_two(input: &str, target: usize) -> usize {
+    let numbers: Vec<usize> = parse_lines(input).collect();
+
+    let result = (2..).find_map(|window_size| {
+        numbers
+            .windows(window_size)
+            .find(|win| win.iter().sum::<usize>() == target)
+    });
+
+    result
+        .map(|v| v.iter().min().unwrap() + v.iter().max().unwrap())
+        .unwrap_or(0)
 }
 
 #[cfg(test)]
 mod tests {
     use super::{star_one, star_two};
+    const INPUT: &'static str = "35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
 
     #[test]
     fn test_star_one() {
-        assert_eq!(star_one(""), 1)
+        assert_eq!(star_one(INPUT, 5), 127);
     }
 
     #[test]
     fn test_star_two() {
-        assert_eq!(star_two(""), 1)
+        assert_eq!(star_two(INPUT, 127), 62);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -261,8 +261,8 @@ mod tests {
 
         let input = load_file("day09.txt");
 
-        assert_eq!(star_one(&input), 1);
-        assert_eq!(star_two(&input), 1);
+        assert_eq!(star_one(&input, 25), 776203571);
+        assert_eq!(star_two(&input, 776203571), 104800569);
     }
 
     #[test]
